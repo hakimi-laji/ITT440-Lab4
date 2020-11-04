d847d4d9d59b851f7d5ab1bb5d38d0c1724e629d
Hakimi Laji <2019290708@isiswa@uitm.edu.my>
2020-11-04 03:54:51 +0000

    SERVER codes for Chat

--- chatServer.c
+++ chatServer.c
@@ -1,96 +1,68 @@
-/* Code unfinished. Not working. Will try later. */
-
-#include <stdio.h> 
-#include <netdb.h> 
-#include <netinet/in.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
-#define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
-
-// Function designed for chat between client and server. 
-void func(int sockfd) 
-{ 
-	char buff[MAX]; 
-	int n; 
-	// infinite loop for chat 
-	for (;;) { 
-		bzero(buff, MAX); 
-
-		// read the message from client and copy it in buffer 
-		read(sockfd, buff, sizeof(buff)); 
-		// print buffer which contains the client contents 
-		printf("From client: %s\t To client : ", buff); 
-		bzero(buff, MAX); 
-		n = 0; 
-		// copy server message in the buffer 
-		while ((buff[n++] = getchar()) != '\n') 
-			; 
-
-		// and send that buffer to client 
-		write(sockfd, buff, sizeof(buff)); 
-
-		// if msg contains "Exit" then server exit and chat ended. 
-		if (strncmp("exit", buff, 4) == 0) { 
-			printf("Server Exit...\n"); 
-			break; 
-		} 
-	} 
-} 
-
-// Driver function 
-int main() 
-{ 
-	int sockfd, connfd, len; 
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and verification 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	servaddr.sin_port = htons(PORT); 
-
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-		printf("socket bind failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully binded..\n"); 
-
-	// Now server is ready to listen and verification 
-	if ((listen(sockfd, 5)) != 0) { 
-		printf("Listen failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Server listening..\n"); 
-	len = sizeof(cli); 
-
-	// Accept the data packet from client and verification 
-	connfd = accept(sockfd, (SA*)&cli, &len); 
-	if (connfd < 0) { 
-		printf("server acccept failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("server acccept the client...\n"); 
-
-	// Function for chatting between client and server 
-	func(connfd); 
-
-	// After chatting close the socket 
-	close(sockfd); 
-} 
+//TCP server
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<string.h>
+#include<netdb.h>
+#include<stdlib.h>
+int main()
+{
+	char buf[100];
+	int k;
+	socklen_t len;
+	int sock_desc,temp_sock_desc;
+	struct sockaddr_in server,client;
+	memset(&server,0,sizeof(server));
+	memset(&client,0,sizeof(client));
+	sock_desc=socket(AF_INET,SOCK_STREAM,0);
+	if(sock_desc==-1)
+	{
+		printf("Error in socket creation");
+		exit(1);
+	}
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=inet_addr("127.0.0.1");
+	server.sin_port=3002;
+	k=bind(sock_desc,(struct sockaddr*)&server,sizeof(server));
+	if(k==-1){
+		printf("Error in binding");
+		exit(1);
+	}
+	k=listen(sock_desc,20);
+	if(k==-1)
+	{
+		printf("Error in listening");
+		exit(1);
+	}
+	len=sizeof(client);//VERY IMPORTANT
+	
+	temp_sock_desc=accept(sock_desc,(struct sockaddr*)&client,&len);//VERY //IMPORTANT
+	if(temp_sock_desc==-1)
+	{
+		printf("Error in temporary socket creation");
+		exit(1);
+	}
+	while(1)
+	{
+		k=recv(temp_sock_desc,buf,100,0);
+		if(k==-1)
+		{
+			printf("Error in receiving");
+			exit(1);
+		}
+		printf("Message got from client is : %s",buf);
+		printf("\nEnter data to be send to client: ");
+		fgets(buf,100,stdin);
+		if(strncmp(buf,"end",3)==0)
+		break;
+		k=send(temp_sock_desc,buf,100,0);
+		if(k==-1)
+		{
+			printf("Error in sending");
+			exit(1);
+		}
+	}
+	close(temp_sock_desc); 
+	exit(0);
+	return 0;
+}
